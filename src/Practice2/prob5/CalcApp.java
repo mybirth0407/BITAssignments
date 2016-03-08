63f0702ed46807fb2610319aeb505e51e6193b66
mybirth0407 <mybirth0407@gmail.com>
2016-03-08 22:59:40 +0900

    루프, quit 추가

--- src/Practice2/prob5/CalcApp.java
+++ src/Practice2/prob5/CalcApp.java
@@ -4,37 +4,46 @@ import java.util.Scanner;
 
  public class CalcApp {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("두 정수와 연산자 입력: ");
         /* 입력 예시 2 3 +
          * 결과 5 */
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        String operand = sc.next();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(">> ");
+            String input = sc.nextLine();
 
-        if (operand.equals("+")) {
-            Arith a = new Add();
-            a.setValue(x, y);
-            System.out.println(a.calculate());
-        }
-        else if (operand.equals("-")) {
-            Arith s = new Sub();
-            s.setValue(x, y);
-            System.out.println(s.calculate());
-        }
-        else if (operand.equals("/")) {
-            Arith d = new Div();
-            d.setValue(x, y);
-            System.out.println(d.calculate());
-        }
-        else if (operand.equals("*")) {
-            Arith m = new Mul();
-            m.setValue(x, y);
-            System.out.println(m.calculate());
-        }
-        else {
-            System.out.println("제대로 입력하렴");
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String str[] = input.split(" ");
+            int x = Integer.parseInt(str[0]);
+            String operand = str[1];
+            int y = Integer.parseInt(str[2]);
+
+            if (operand.equals("+")) {
+                Arith a = new Add();
+                a.setValue(x, y);
+                System.out.println(a.calculate());
+            }
+            else if (operand.equals("-")) {
+                Arith s = new Sub();
+                s.setValue(x, y);
+                System.out.println(s.calculate());
+            }
+            else if (operand.equals("/")) {
+                Arith d = new Div();
+                d.setValue(x, y);
+                System.out.println(d.calculate());
+            }
+            else if (operand.equals("*")) {
+                Arith m = new Mul();
+                m.setValue(x, y);
+                System.out.println(m.calculate());
+            }
+            else {
+                System.out.println("제대로 입력하렴");
+            }
         }
     }
 }
